bbf146b131c85c6c7f46bac5789b96f397b82ac4
Your name <ruszabarov@gmail.com>
2025-01-21 10:01:55 -0500

    Initial Commit

---
+++ ios-dev/ios_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_devApp.swift
+//  ios-dev
+//
+//  Created by Ruslan Zabarov on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
